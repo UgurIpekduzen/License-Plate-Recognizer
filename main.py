d43a162c180fa9236dfe5c0e3aca34fb238c10fa
Uğur İpekdüzen <u.ipkdzn@gmail.com>
2019-07-11 16:45:53 +0300

    v1.0.0

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import cv2
 import os
+import pytesseract as tess
 import time
+from PIL import Image
 from skimage.io import imread
 from skimage.filters import threshold_otsu
 import matplotlib.pyplot as plt
 
+tess.pytesseract.tesseract_cmd = 'D:/Tesseract-OCR/tesseract.exe'
 count = 0
-vidcap = cv2.VideoCapture(0)
+cam = cv2.VideoCapture(0)
 capDuration = 5
 startTime = time.time()
-while int(time.time() - startTime) != capDuration:
-    success, frame = vidcap.read()
+frames = []
+
+while True:
+    success, frame = cam.read()
+    frames.append(frame)
     cv2.imwrite("./frames/frame%d.jpg" % count, frame)
     print('Screenshot alındı!')
     count = count + 1
     time.sleep(1)
+    if int(time.time() - startTime) == capDuration:
+        break
 
-vidcap.release()
+cam.release()
 cv2.destroyAllWindows()
 #
 #   Tüm kodlar buraya...
@@ -32,8 +40,18 @@ binaryImage = imageGray > thresholdValue
 ax2.imshow(binaryImage, cmap="gray")
 plt.show()
 
+listExtacts = []
+for fr in frames:
+    new_im = Image.fromarray(fr)
+    test = tess.image_to_string(new_im)
+    listExtacts.append(test)
+    print(test)
+
+
 #işlemler bitince sil
 while count != 0:
     count = count - 1
     os.unlink("./frames/frame%d.jpg" % count)
     print(count)
+
+print(time.time() - startTime)
