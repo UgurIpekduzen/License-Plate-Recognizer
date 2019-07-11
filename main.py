8f4a4cc4de823a8dd4ed5a81e75a2901237b38a1
Uğur İpekdüzen <u.ipkdzn@gmail.com>
2019-07-11 13:06:37 +0300

    Gray Image to Binary Image

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import cv2
 import os
 import time
+from skimage.io import imread
+from skimage.filters import threshold_otsu
+import matplotlib.pyplot as plt
 
 vidcap = cv2.VideoCapture(0)
-filePath = "E:/Repos/License Plate Recognizer/frames/frame.jpg"
+filePath = "./frames/frame.jpg"
 capDuration = 5
 startTime = time.time()
 while True:
-    success, image = vidcap.read()
+    success, frame = vidcap.read()
+
     if(int(time.time() - startTime) == capDuration):
-        cv2.imwrite(filePath, image)
+        cv2.imwrite(filePath, frame)
         print('Screenshot alındı!')
         break
 
@@ -18,6 +22,16 @@ cv2.destroyAllWindows()
 #
 #   Tüm kodlar buraya...
 #
+
+image = imread(filePath, as_gray="True")
+
+imageGray = image * 255
+fig, (ax1, ax2) = plt.subplots(1,2)
+ax1.imshow(imageGray, cmap="gray")
+thresholdValue = threshold_otsu(imageGray)
+binaryImage = imageGray > thresholdValue
+ax2.imshow(binaryImage, cmap="gray")
+plt.show()
 
 #işlemler bitince sil
 os.unlink(filePath)
