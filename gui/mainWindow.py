6325902b9d0fc2321ba40c3a40911192d82b35f7
Uğur İpekdüzen <u.ipkdzn@gmail.com>
2019-08-08 08:21:14 +0300

    GUI is redesigned
    Additons
    - Previous Plates, Camera Capture, Reading Plate,  Matching Vehicle Info, Pass Icons Group Boxes
    - Table Widget for Previous Plates Group Box
    - Icon and Text Labels for Pass Icons Group Box
    - Progress Bar for countdown, Text Label and Icon Label, Push Button to start reading in Reading Plate Group Box
    -  Plate Text, Register Status and Black List Status Text Label, Push Button to query result in database, Push Button to add new object to the database for Matching Vehicle Info Group Box
    - Image Label for WebCam Capture Group Box

--- gui/mainWindow.py
+++ gui/mainWindow.py
@@ -3,6 +3,7 @@ sys.path.append("E:/Repos/License-Plate-Recognizer-GitHub/src/database")
 sys.path.append("E:/Repos/License-Plate-Recognizer-GitHub/src/read_and edit_images")
 import SQLQueries
 import ReadImage
+import qtawesome as qta
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -20,8 +21,13 @@ class MainWindow(QWidget):
         self.LP = ""
         self.timer = QTimer()
         # self.timer.timeout.connect(self.viewCam)
-        self.ui.StartReadingButton.clicked.connect(self.readLicensePlateFromWebCam)
-        self.ui.MatchingVehicleInfoPushButton.clicked.connect(self.getVehicleInfoFromDatabase)
+        self.ui.ButtonStartReading.clicked.connect(self.readLicensePlateFromWebCam)
+        self.ui.ButtonQueryVehicleInfoInDB.clicked.connect(self.getVehicleInfoFromDatabase)
+
+        self.ui.LabelRegisteredIcon.setPixmap(QPixmap("E:/Repos/License-Plate-Recognizer-GitHub/gui/icon/registered.png"))
+        self.ui.LabelUnrecognizedIcon.setPixmap(QPixmap("E:/Repos/License-Plate-Recognizer-GitHub/gui/icon/unknown.png"))
+        self.ui.LabelBlackListedIcon.setPixmap(QPixmap("E:/Repos/License-Plate-Recognizer-GitHub/gui/icon/blacklisted.png"))
+        self.ui.LabelGuestIcon.setPixmap(QPixmap("E:/Repos/License-Plate-Recognizer-GitHub/gui/icon/guest.png"))
 
     # def viewCam(self):
     #     ret, image = self.cap.read()
@@ -31,18 +37,38 @@ class MainWindow(QWidget):
     #     qImg = QImage(image.data, width, height, step, QImage.Format_RGB888)
     #     self.ui.FrameLabel.setPixmap(QPixmap.fromImage(qImg))
 
+    def setPassIcons(self):
+        self.ui.LabelRegisteredIcon.setPixmap(
+            QPixmap("E:/Repos/License-Plate-Recognizer-GitHub/gui/icon/registered.png"))
+        self.ui.LabelUnrecognizedIcon.setPixmap(
+            QPixmap("E:/Repos/License-Plate-Recognizer-GitHub/gui/icon/unknown.png"))
+        self.ui.LabelBlackListedIcon.setPixmap(
+            QPixmap("E:/Repos/License-Plate-Recognizer-GitHub/gui/icon/blacklisted.png"))
+        self.ui.LabelGuestIcon.setPixmap(QPixmap("E:/Repos/License-Plate-Recognizer-GitHub/gui/icon/guest.png"))
+
     def readLicensePlateFromWebCam(self):
-        self.ui.MatchingVehicleInfoTextBrowser.setText("")
+        self.ui.LabelReadingLPText.setText("")
+        # self.ui.MatchingVehicleInfoTextBrowser.setText("")
         self.LP = ReadImage.fromWebCam()
         if len(self.LP) > 0:
-            self.ui.ReadingLPIconLabel.setPixmap(QPixmap("E:\Repos\License-Plate-Recognizer-GitHub\gui\icon\checked.png"))
+            self.ui.LabelReadingLPIcon.setPixmap(QPixmap("E:\Repos\License-Plate-Recognizer-GitHub\gui\icon\checked.png"))
         else:
-            self.ui.ReadingLPIconLabel.setPixmap(QPixmap("E:\Repos\License-Plate-Recognizer-GitHub\gui\icon\cancel.png"))
-        self.ui.ReadingLPTextBrowser.setText(self.LP)
-
+            self.ui.LabelReadingLPIcon.setPixmap(QPixmap("E:\Repos\License-Plate-Recognizer-GitHub\gui\icon\cancel.png"))
+        self.ui.LabelReadingLPText.setText(self.LP)
 
     def getVehicleInfoFromDatabase(self):
-        self.ui.MatchingVehicleInfoTextBrowser.setText(SQLQueries.showFoundVehicleDBInfo(self.LP))
+        foundVehicle = SQLQueries.selectByLicensePlate(self.LP)
+        self.ui.LabelLPText.setText("PLAKA")
+        if(foundVehicle):
+            self.ui.LabelLPText.setText(foundVehicle[0][0])
+            self.ui.LabelRegisterStatusText.setText("Evet" if foundVehicle[0][1] == 1 else "Hayır")
+            self.ui.LabelBLStatusText.setText("Evet" if foundVehicle[0][2] == 1 else "Hayır")
+
+            self.ui.LabelLabelNotificationMesssageIcon.setPixmap(QPixmap("E:\Repos\License-Plate-Recognizer-GitHub\gui\icon\checked2.png"))
+            self.ui.LabelNotificationMesssage.setText("Eşleşen bir kayıt bulundu!")
+        else:
+            self.ui.LabelLabelNotificationMesssageIcon.setPixmap(QPixmap("E:\Repos\License-Plate-Recognizer-GitHub\gui\icon\cancel2.png"))
+            self.ui.LabelNotificationMesssage.setText("Eşleşen herhangi bir kayıt bulunamadı!")
         self.LP = ""
 
 if __name__ == '__main__':
